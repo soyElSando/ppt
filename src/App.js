4e7915261984690897a9a1a69efb81b0d487b3d6
soyElSando <german.tito.sandoval@gmail.com>
2022-09-25 13:42:23 -0300

    PPT v0.9

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  
+  const [resultado, setResultado] = useState("Haz click para jugar");
+
+  const play = (valor)=>{
+    let valorCompu="";
+    const jugadaCompu = Math.floor(Math.random() * 3); //genera un valor random para setear el valorCompu
+    switch(jugadaCompu){
+      case 0:
+        valorCompu="piedra";
+        break;
+      case 1:
+        valorCompu="papel";
+        break;
+      default:
+        valorCompu="tijera";
+    }
+    //if (jugadaCompu===0){
+    //  valorCompu="piedra";
+    //}
+    //else if (jugadaCompu===1){
+    //  valorCompu="papel";
+    //}else {
+    //  valorCompu="tijera";
+    //}
+    console.log(valor+" vs "+valorCompu); //veo la competencia
+    if (valor===valorCompu){              //seteo el resultado
+      setResultado("Empataste contra "+valorCompu);
+    }
+    else if((valor==="piedra"&&valorCompu==="papel")||(valor==="tijera"&&valorCompu==="piedra")||(valor==="papel"&&valorCompu==="tijera")){
+      setResultado("Perdiste contra "+valorCompu);
+    }
+    else{
+      setResultado("Ganaste contra "+valorCompu);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App ">
+      <header className="App-header text-white m-1 p-1">
+        <h2>PIEDRA, PAPEL O TIJERA</h2>
+        <button onClick={()=>play("piedra")} className="btn-success text-white m-1 p-1">PIEDRA</button>
+        <button onClick={()=>play("papel")} className="btn-warning text-white m-1 p-1">PAPEL</button>
+        <button onClick={()=>play("tijera")} className="btn-danger text-white m-1 p-1">TIJERA</button>
+        <div className="bg-dark text-white m-1 p-1">{resultado}</div>   
       </header>
     </div>
   );
 }
 
 export default App;
+ 
